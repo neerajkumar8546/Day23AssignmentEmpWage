3cb1bbd83e2f27df06786d8de0870a07d4b7b526
unknown <neerajkumar8546@gmail.com>
2020-06-17 18:38:54 +0530

    Cal full time empWage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,7 +1,18 @@
 public class EmployeeWageBuilder
 {
+
 	public static void main(String[] args)
 	{
-		System.out.println("* Welcome to Employee Wage Computation *");
+			int is_Full_Time=1;
+		   int emp_Rate_Per_Hrs=20;
+			int empWage=0,empHrs=0;
+			int check=(int)Math.floor(Math.random()*10)%2;
+			if(check==is_Full_Time)
+				empHrs=8;
+			else
+				empHrs=0;
+
+			empWage=empHrs*emp_Rate_Per_Hrs;
+			System.out.println("EmpWage = "+empWage);
 	}
 }
