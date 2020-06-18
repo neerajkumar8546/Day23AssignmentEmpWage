7b4821676290e42d96cf38e9ed9c16f8597cc34e
unknown <neerajkumar8546@gmail.com>
2020-06-18 22:36:11 +0530

    Store Daily Wage And Total Wage for Company

---
+++ EmpWageStoreWage_TotalWage.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+interface ICompanyEmpwage{
+	public void addCompanyEmpWage(String company_name,int emp_rate_per_hour,int working_days,int max_hours_per_month);
+	public void computeEmpwage();
+
+}
+
+class CompanyEmpwage{
+	final String company_name;
+    final int emp_rate_per_hour;
+	final int working_days;
+	final int max_hours_per_month;
+	private int totalEmpwge;
+
+	public CompanyEmpwage(String company_name,int emp_rate_per_hour,int working_days,int max_hours_per_month){
+		this.company_name = company_name;
+		this.emp_rate_per_hour = emp_rate_per_hour;
+		this.working_days = working_days;
+		this.max_hours_per_month = max_hours_per_month;
+	}
+	public void settotalEmpwage(int totalEmpwge){
+		this.totalEmpwge=totalEmpwge;
+	}
+	public String toString(){
+		return "Total Empwage for company " +company_name+":"+totalEmpwge;
+	}
+}
+
+public class EmpWageStoreWage_TotalWage implements ICompanyEmpwage {
+	public static final int Part_Time = 1;
+	public static final int Full_Time = 2;
+
+	List<CompanyEmpwage> companyEmpwageArray = new ArrayList<CompanyEmpwage> ();
+	List<Integer> daily_wages = new ArrayList<Integer> ();
+	public void addCompanyEmpWage(String company_name,int emp_rate_per_hour,int working_days,int max_hours_per_month){
+		CompanyEmpwage e = new CompanyEmpwage(company_name , emp_rate_per_hour , working_days , max_hours_per_month);
+		companyEmpwageArray.add(e);
+	}
+	public void computeEmpwage(){
+		for(int i = 0; i < companyEmpwageArray.size(); i++){
+			companyEmpwageArray.get(i).settotalEmpwage(this.computeEmpwage(companyEmpwageArray.get(i)));
+			System.out.println(companyEmpwageArray.get(i));
+		}
+	}
+
+	public void printDailyWage(){
+		for(int i=0;i<daily_wages.size();i++){
+			System.out.println(daily_wages.get(i));
+		}
+	}
+
+	public int computeEmpwage(CompanyEmpwage companyEmpwageUC11){
+		int hrs=0 , totalhrs=0, totalworkingdays=0;
+		while(totalhrs <= companyEmpwageUC11.max_hours_per_month && totalworkingdays < companyEmpwageUC11.working_days){
+			totalworkingdays++;
+			int empcheck = (int) Math.floor(Math.random()*10)%3;
+			switch(empcheck){
+				case Part_Time:
+					hrs=4;
+					break;
+				case Full_Time:
+					hrs=8;
+					break;
+				default:
+					hrs=0;
+			}
+			totalhrs += hrs;
+			daily_wages.add(hrs*20);
+			System.out.println("Day :" + totalworkingdays + " " + "Emphrs :" + hrs + "Daily wage=" + (hrs*20));
+		}
+		return totalhrs * companyEmpwageUC11.emp_rate_per_hour;
+	}
+	public static void main(String[] args){
+		EmpWageStoreWage_TotalWage empwagrbuilder = new EmpWageStoreWage_TotalWage();
+		empwagrbuilder.addCompanyEmpWage("DMart",20,22,100);
+		empwagrbuilder.addCompanyEmpWage("Sephora",40,26,200);
+		empwagrbuilder.computeEmpwage();
+		System.out.println("Daily wages:");
+		empwagrbuilder.printDailyWage();
+
+	}
+
+}
